29dec02e918df160e8cba24ed45300b51485b151
Kra1o5 <kra1o5x@gmail.com>
2016-11-23 20:36:31 +0100

    Merge 1.5.1 release aquaris-X5-plus-1.5.1_20161117-1500
    Based off CAF tag: LA.BR.1.3.4-08410-8976.0

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -5583,8 +5583,8 @@ void runin_work(struct smbchg_chip *chip, int batt_capacity)
 			dev_err(chip->dev,
 				"Couldn't disenable charge rc=%d\n", rc);
 	} else {
-		if (batt_capacity <= 50) {
-		pr_debug("smbcharge_get_prop_batt_capacity <= 50\n");
+		if (batt_capacity <= 60) {
+		pr_debug("smbcharge_get_prop_batt_capacity <= 60\n");
 		rc = smbchg_usb_suspend(chip,false);
 		if (rc)
 			dev_err(chip->dev,
